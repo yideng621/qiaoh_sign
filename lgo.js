28ec74065f50150aa8e46c7cc20d01b99e4564c8
yideng621 <34930584+yideng621@users.noreply.github.com>
2020-11-12 09:29:36 +0800

    Update lgo.js

--- lgo.js
+++ lgo.js
@@ -1,2 +1,11 @@
-    console.log(`您的cookie选择的是用换行隔开\n`)
+    console.log(`您的cookie选择的是用换行隔开\n`);
     CookieJDs = process.env.JD_COOKIE.split('\n');
+
+    console.log(`您的东东农场互助码`);
+    FruitShareCodes = process.env.FruitShareCodes;
+
+    console.log(`您的东东萌宠互助码`);
+    PETSHARECODES = process.env.PETSHARECODES;
+
+    console.log(`种豆得豆互助码`);
+    PLANT_BEAN_SHARECODES = process.env.PLANT_BEAN_SHARECODES;
