6c3a324c70ab622f879d7e26e235139bd8c21f78
yideng621 <34930584+yideng621@users.noreply.github.com>
2020-11-12 10:33:50 +0800

    Update lgo.py

--- lgo.py
+++ lgo.py
@@ -5,9 +5,12 @@ import os
 
 FruitShareCodes=os.environ["FruitShareCodes"]
 print("您的东东农场互助码")
+print(FruitShareCodes)
 
 PETSHARECODES=os.environ["PETSHARECODES"]
 print("您的东东萌宠互助码")
+print(PETSHARECODES)
 
 PLANT_BEAN_SHARECODES=os.environ["PLANT_BEAN_SHARECODES"]
 print("您的种豆得豆互助码")
+print(PLANT_BEAN_SHARECODES)
