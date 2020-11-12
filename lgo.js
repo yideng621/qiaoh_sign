bddb1c9d11ded487b0eff7eb203fff0df2f5a8b8
yideng621 <34930584+yideng621@users.noreply.github.com>
2020-11-12 09:55:34 +0800

    Update lgo.js

--- lgo.js
+++ lgo.js
@@ -1,9 +1,12 @@
+##`\n【您的互助码shareCode】 ${$.petInfo.shareCode}\n`
 
-    console.log(`您的东东农场互助码`);
+    
     FruitShareCodes = process.env.FruitShareCodes;
+    console.log(`\n您的东东农场互助码 ${$.FruitShareCodes}\n`);
 
-    console.log(`您的东东萌宠互助码`);
+    
     PETSHARECODES = process.env.PETSHARECODES;
+    console.log(`\n您的东东萌宠互助码  ${$.PETSHARECODES}\n`);
 
-    console.log(`种豆得豆互助码`);
+    console.log(`\n种豆得豆互助码 ${$.process.env.PLANT_BEAN_SHARECODES} \n`);
     PLANT_BEAN_SHARECODES = process.env.PLANT_BEAN_SHARECODES
