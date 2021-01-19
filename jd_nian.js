967f357443323e5a81f5038cc545b67ea7d39885
yideng621 <34930584+yideng621@users.noreply.github.com>
2021-01-19 23:20:16 +0800

    Update jd_nian.js

--- jd_nian.js
+++ jd_nian.js
@@ -806,7 +806,7 @@ function pkAssignGroup(inviteId) {
 function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
-    $.get({url: `https://code.chiang.fun/api/v1/jd/jdnian/read/${randomCount}/`, 'timeout': 10000}, (err, resp, data) => {
+    $.get({url: `https://code.chiang.fun/api/v1/jd/jdnian/`, 'timeout': 10000}, (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
